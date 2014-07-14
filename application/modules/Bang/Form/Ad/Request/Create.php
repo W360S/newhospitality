e2cc0774d0fd53fede0b6af35318d32c69435361
Vu Ngoc Bang <bangvn.dng@gmail.com>
2014-07-15 01:02:25 +0700

    update check point of feature 286

--- application/modules/Bang/Form/Ad/Request/Create.php
+++ application/modules/Bang/Form/Ad/Request/Create.php
@@ -19,8 +19,8 @@ class Bang_Form_Ad_Request_Create extends Engine_Form {
         $viewer = Engine_Api::_()->user()->getViewer();
         $viewer_id = $viewer->getIdentity();
 
+        /*
         $title = Zend_Registry::get('Zend_Translate')->_('Submit your new Advertisement');
-
         if (!empty($viewer_id)) {
             $this->setTitle($title)
                     ->setDescription('Please send us your request for posting Advertisement')
@@ -33,8 +33,9 @@ class Bang_Form_Ad_Request_Create extends Engine_Form {
             $this->setAttrib('name', 'bang_ad_create');
             $this->loadDefaultDecorators();
             $this->getDecorator('Description')->setOption('escape', false);
-        }
+        }*/
 
+        // Head of form section
         $this->addElement('Text', 'ad_title', array(
             'label' => 'Title*',
             'filters' => array(
@@ -44,6 +45,13 @@ class Bang_Form_Ad_Request_Create extends Engine_Form {
         
         $this->addElement('Text', 'ad_subtitle', array(
             'label' => 'Sub Title*',
+            'filters' => array(
+                'StripTags',
+                new Engine_Filter_Censor(),
+        )));
+
+        $this->addElement('Text', 'ad_link', array(
+            'label' => 'Link*',
             'filters' => array(
                 'StripTags',
                 new Engine_Filter_Censor(),
@@ -59,23 +67,44 @@ class Bang_Form_Ad_Request_Create extends Engine_Form {
             ),
         ));
 
-        $this->addElement('Text', 'ad_email', array(
-            'label' => 'Email*',
-            'required' => true,
-            'allowEmpty' => false,
-            'validators' => array(
-                array('NotEmpty', true),
-                array('EmailAddress', true))
-        ));
-        
-            $this->addElement('Text', 'ad_phone', array(
-            'label' => 'Phone*',
-            'required' => true,
-            'allowEmpty' => false,
-            'validators' => array(
-                array('NotEmpty', true))
+        // Ads campagn section
+
+        $potition_options = array(
+            "1" => "Cộng đồng --- 60.000 vnd / ngày",
+            "2" => "Việc làm --- 50.000 vnd / ngày",
+            "3" => "Thư viện --- 30.000 vnd / ngày",
+            "4" => "Hỏi đáp --- 30.000 vnd / ngày",
+        );
+        // Position
+        $this->addElement('Select', 'ad_position', array(
+        'label' => 'Position',
+        'multiOptions' => $potition_options,
+        'value' => key($potition_options),
         ));
 
+        // Start time
+        $start = new Engine_Form_Element_CalendarDateTime('ad_start');
+        $start->setLabel("Start Time");
+        $this->addElement($start);
+
+        // End time
+        $end = new Engine_Form_Element_CalendarDateTime('ad_end');
+        $end->setLabel("End Time");
+        $this->addElement($end);
+
+
+        $this->addElement('Text', 'ad_total', array(
+            'label' => 'Thanh tien',
+            'allowEmpty' => false,
+            'required' => true,
+            'filters' => array(
+                'StripTags',
+                new Engine_Filter_Censor(),
+                new Engine_Filter_StringLength(array('max' => '63')),
+        )));
+
+
+        // User contact section
         $this->addElement('Text', 'ad_name', array(
             'label' => 'Full Name*',
             'allowEmpty' => false,
@@ -86,6 +115,25 @@ class Bang_Form_Ad_Request_Create extends Engine_Form {
                 new Engine_Filter_StringLength(array('max' => '63')),
         )));
 
+        $this->addElement('Text', 'ad_email', array(
+            'label' => 'Email*',
+            'required' => true,
+            'allowEmpty' => false,
+            'validators' => array(
+                array('NotEmpty', true),
+                array('EmailAddress', true))
+        ));
+        
+        $this->addElement('Text', 'ad_phone', array(
+            'label' => 'Phone*',
+            'required' => true,
+            'allowEmpty' => false,
+            'validators' => array(
+                array('NotEmpty', true))
+        ));
+
+        
+        // End form
         $this->addElement('Button', 'submit', array(
             'label' => 'Post Advertisement',
             'type' => 'submit',
--- application/modules/Bang/views/scripts/index/create.tpl
+++ application/modules/Bang/views/scripts/index/create.tpl
@@ -1,4 +1,48 @@
 <?php echo $this->form->render($this) ?> 
 <style>
     body#global_page_bang-index-create{padding: 10px}
+    #global_page_bang-index-create .form-elements {min-width: 450px}
+    #global_page_bang-index-create .form-label {width: 29%; float: left;}
+    #global_page_bang-index-create .form-element {width: 69%; float: left;}
 </style>
+<script>
+	jQuery("document").ready(function($){
+		console.log("registered");
+		
+		$("input#ad_end-date").on("change paste keyup", function() {
+		   alert($(this).val()); 
+		});
+
+		function checkVal(){
+		setTimeout(function() {
+
+				console.log("checking value");
+				console.log($("#ad_start-date").val());
+				console.log($("#ad_end-date").val());
+
+				var start = $("#ad_start-date").val();
+				var end = $("#ad_end-date").val();
+				if (start==="" || end ==="") {
+					$("#ad_total").val(0);
+				}else{
+					if(start > end){
+						$("#ad_total").val("start > end");
+					}else{
+						$("#ad_total").val(end - start);
+					}
+					console.log(start > end);
+				}
+
+				checkVal();
+
+			}, 2000);
+		}
+
+		checkVal();
+	});
+
+	
+
+	
+
+</script>
